4fdd9a83d735e280a63049bffe37b4424b3d768a
Signum10 <tsi2k6@yahoo.com>
2021-03-09 16:51:08 +0200

    Finished simple version of Led Cube Slave

---
+++ LC-8x8x8-RGB-00 (Master)/main.asm
@@ -0,0 +1,12 @@
+;
+; LC-8x8x8-RGB-00 (Master).asm
+;
+; Created: 09.03.2021 16:20:47
+; Author : TeoLiliana
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
---
+++ Testing_Board/main.asm
@@ -0,0 +1,12 @@
+;
+; Testing_Board.asm
+;
+; Created: 09.03.2021 16:21:09
+; Author : TeoLiliana
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
