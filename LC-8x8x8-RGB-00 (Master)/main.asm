8c47cc6f705b2dd6e97968f50b9cf1d91f49f08e
Signum10 <tsi2k6@yahoo.com>
2022-03-23 22:17:30 +0200

    More code written for master

--- LC-8x8x8-RGB-00 (Master)/main.asm
+++ LC-8x8x8-RGB-00 (Master)/main.asm
@@ -31,7 +31,86 @@ start:          ldi r16, PORTB_INIT
                 ldi r16, TCCR1B_INIT
                 sts TCCR1B, r16
 
-loop:           rjmp loop
+                ldi YL, low(SLAVE_PROC)
+                ldi YH, high(SLAVE_PROC)
+                ldi r16, SLAVE_PROC_DATA_SIZE
+                ldi r17, 0
+
+                st Y+, r17
+                dec r16
+                brne PC - 2
+
+loop_init:      ldi r16, ~(1 << INT_PIN_SLAVE0)
+                mov CURRENT_SLAVE, r16
+                
+                ldi YL, low(SLAVE_PROC)
+                ldi YH, high(SLAVE_PROC)
+
+loop:           in r16, INT_PINREG
+                and r16, CURRENT_SLAVE
+                brne loop_cont
+
+                ldi r16, DXX_CMD_GET_INT
+                ldi r24, low(DXX_CMD_GET_INT_DATA_SIZE)
+                ldi r25, high(DXX_CMD_GET_INT_DATA_SIZE)
+                rcall tx_slave_get
+
+                lds CURRENT_INT, PACKET_BUFFER
+
+                sbrs CURRENT_INT, DXX_CMD_GET_INT_FLAG_RESET
+                rjmp loop_call_proc
+
+                ldi r16, DXX_CMD_GET_DEVID
+                ldi r24, low(DXX_CMD_GET_DEVID_DATA_SIZE)
+                ldi r25, high(DXX_CMD_GET_DEVID_DATA_SIZE)
+                rcall tx_slave_get
+
+                lds r17, PACKET_BUFFER
+
+                ldi r16, 0
+                std Y + 0, r16
+                std Y + 1, r16
+
+                ldi ZL, low(LUT_devids_proc << 1)
+                ldi ZH, high(LUT_devids_proc << 1)
+
+                lpm r16, Z
+                tst r16
+                breq loop_cont
+                cp r16, r17
+                breq PC + 3
+                adiw Z, 4
+                rjmp PC - 6
+
+                adiw Z, 2
+                lpm r16, Z+
+                std Y + 0, r16
+                lpm r16, Z+
+                std Y + 1, r16
+
+loop_call_proc: ldd ZL, Y + 0
+                ldd ZH, Y + 1
+                adiw Z, 0
+                breq loop_cont
+
+                push YL
+                push YH
+                icall
+                pop YH
+                pop YL
+
+loop_cont:      adiw Y, SLAVE_PROC_ELEMENT_SIZE
+                sec
+                rol CURRENT_SLAVE
+                ldi r16, ~(1 << (INT_PIN_SLAVE5 + 1))
+                cp CURRENT_SLAVE, r16
+                brne loop
+                rjmp loop_init
+
+LUT_devids_proc:
+.db D01_CMD_GET_DEVID_VALUE, 0 \ .dw proc_LC
+.db DTT_CMD_GET_DEVID_VALUE, 0 \ .dw proc_TT
+.db 0
 
 //////////////////////////////////////////////////
 
@@ -82,3 +161,13 @@ txrx_spi:       out SPDR, r16
                 rjmp PC - 2
                 in r16, SPDR
                 ret
+
+////////////////////////////////////////////////// Led Cube
+proc_LC:        ret
+
+////////////////////////////////////////////////// Test Slave
+proc_TT:        sbrc CURRENT_INT, DTT_CMD_GET_INT_FLAG_SEND_PORT
+                rjmp PC
+
+                ;translate slave to port nr and send
+                ldi r16, ~(1 << INT_PIN_SLAVE0)
